05980f18cc73d4a31c8f270a5dc8eb36ee4d29f2
skaltanukiman <java.w.s.a.b.c@gmail.com>
2026-05-08 13:51:43 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
