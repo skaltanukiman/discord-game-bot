af5737b19a571c840f8fa81d8b6cdae05a113743
skaltanukiman <java.w.s.a.b.c@gmail.com>
2026-05-08 22:23:55 +0900

    JSDocの記入

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,11 @@
+/**
+ * 指定した環境変数を取得します。
+ * 環境変数が未設定の場合は例外をスローします。
+ * 
+ * @param name 環境変数名
+ * @returns 環境変数値
+ * @throws Error 環境変数が未設定の場合
+ */
 function getEnv(name: string) {
     const value = process.env[name];
 
