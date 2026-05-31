e401b3f8c950cb57f0c03027b58d871140265519
skaltanukiman <java.w.s.a.b.c@gmail.com>
2026-05-31 16:15:40 +0900

    移設コードの削除

--- src/services/steamService.ts
+++ src/services/steamService.ts
@@ -478,25 +478,6 @@ export async function fetchAllSteamStoreApps(options: SteamAppListOptions = {}):
 }
 
 /**
- * Steam Store に登録されているゲーム一覧を全件取得する
- * 
- * DLC、ソフトウェア、動画、ハードウェアは除外し、
- * ゲームとして扱われるSteamアプリのみを取得する。
- * 
- * @returns Steam Store上のゲーム一覧
- */
-// export async function fetchAllSteamGames(): Promise<SteamStoreApp[]> {
-//     return await fetchAllSteamStoreApps({
-//         includeGames: true,
-//         includeDlc: false,
-//         includeSoftware: false,
-//         includeVideos: false,
-//         includeHardware: false,
-//         maxResults: 50000
-//     });
-// }
-
-/**
  * Steam Store のゲーム一覧を取得する
  * 
  * 有効なキャッシュがある場合はキャッシュを返し、
