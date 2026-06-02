c6e89f19d9d333653584a14f59f541e093fed962
skaltanukiman <java.w.s.a.b.c@gmail.com>
2026-06-02 21:05:11 +0900

    embed送信処理の前処理に 空配列だった場合の処理を追加

--- src/services/embedService.ts
+++ src/services/embedService.ts
@@ -3,6 +3,7 @@ import { createButton, createEmbed } from "../util/embedUtil.js";
 import { TextChannel } from "discord.js";
 import { setTimeout } from "timers/promises";
 import { generalSetting } from "../config/setting.js";
+import { logger } from "../util/logger.js";
 
 /**
  * ゲーム詳細情報をDiscordチャンネルへ順番に送信する
@@ -12,18 +13,30 @@ import { generalSetting } from "../config/setting.js";
  * 
  * @param gameDetails 送信対象のゲーム詳細情報配列
  * @param channel メッセージ送信先のDiscordテキストチャンネル
+ * @returns Embed送信件数
  */
-export async function sendGameDetailsToChannel(gameDetails:ExtendedSteamGameDetail[], channel: TextChannel) {
+export async function sendGameDetailsToChannel(gameDetails:ExtendedSteamGameDetail[], channel: TextChannel): Promise<number> {
+    if (gameDetails.length === 0) {
+        logger.info("ゲーム情報が空のため送信処理は行いません");
+        return 0;
+    }
 
+    if (!generalSetting.send.gameDetails) {
+        logger.info("ゲーム詳細送信設定が無効のため、Discord送信は行わずEmbed生成のみ実行します");
+    }
+
+    let sendCount: number = 0;
     for (const game of gameDetails) {
         const embed = await createEmbed(game);
         const buttons = createButton(game);
 
         if (generalSetting.send.gameDetails) {
             await channel.send({ embeds: [embed], components: [buttons] });
+            sendCount++;
         }
         
         await setTimeout(1000);
     }
-    
+
+    return sendCount;
 }
